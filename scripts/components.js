161e24b4e7432f774d6959103d29352a21bebe70
sairaj mote <sairajmote3@gmail.com>
2022-09-06 19:42:27 +0530

    UI/UX improvements

--- scripts/components.js
+++ scripts/components.js
@@ -2418,40 +2418,32 @@ smMenu.innerHTML = `
     margin: 0;
     -webkit-box-sizing: border-box;
             box-sizing: border-box;
-} 
-.menu{
-    display: -ms-grid;
-    display: grid;
-    place-items: center;
-    position: relative;
-    height: 2rem;
-    width: 2rem;
-    outline: none;
-    background-color: transparent;
-    border: none;
-    cursor: pointer;
-    padding: 0.7rem;
-    -webkit-transition: background 0.3s;
-    -o-transition: background 0.3s;
-    transition: background 0.3s;
-    border-radius: 2rem;
 }
-.icon {
-    position: absolute;
-    fill: rgba(var(--text-color), 0.7);
-    height: 1.2rem;
-    width: 1.2rem;
-}      
 :host{
     display: -webkit-inline-box;
     display: -ms-inline-flexbox;
     display: inline-flex;
+
 }
-.hide{
-    opacity: 0;
-    pointer-events: none;
-    user-select: none;
+.menu{
+    display: -ms-grid;
+    display: grid;
+    place-items: center;
+    height: 2rem;
+    width: 2rem;
+    outline: none;
 }
+.icon {
+    position: absolute;
+    fill: rgba(var(--text-color), 0.7);
+    height: 2.4rem;
+    width: 2.4rem;
+    padding: 0.5rem;
+    border-radius: 2rem;
+    -webkit-transition: background 0.3s;
+    -o-transition: background 0.3s;
+    transition: background 0.3s;
+}      
 .select{
     position: relative;
     display: -webkit-box;
@@ -2465,7 +2457,7 @@ smMenu.innerHTML = `
     width: 100%;
     -webkit-tap-highlight-color: transparent;
 }
-.menu:focus,
+.menu:focus .icon,
 .focused{
     background: rgba(var(--text-color), 0.1); 
 }
@@ -2476,7 +2468,8 @@ smMenu.innerHTML = `
     right: 0;
 }
 .options{
-    padding: 0.5rem 0;
+    top: 100%;
+    padding: 0.3rem;
     overflow: hidden auto;
     position: absolute;
     display: -webkit-box;
@@ -2485,55 +2478,40 @@ smMenu.innerHTML = `
     min-width: -webkit-max-content;
     min-width: -moz-max-content;
     min-width: max-content;
-    -webkit-transform: translateY(-1rem);
-        -ms-transform: translateY(-1rem);
-            transform: translateY(-1rem);
     -webkit-box-orient: vertical;
     -webkit-box-direction: normal;
         -ms-flex-direction: column;
             flex-direction: column;
-    background: rgba(var(--foreground-color), 1);
-    -webkit-transition: opacity 0.3s, -webkit-transform 0.3s;
-    transition: opacity 0.3s, -webkit-transform 0.3s;
-    -o-transition: opacity 0.3s, transform 0.3s;
-    transition: opacity 0.3s, transform 0.3s;
-    transition: opacity 0.3s, transform 0.3s, -webkit-transform 0.3s;
-    border: solid 1px rgba(var(--text-color), 0.2);
-    border-radius: 0.3rem;
-    z-index: 2;
-    -webkit-box-shadow: 0.4rem 0.8rem 1.2rem #00000030;
-            box-shadow: 0.4rem 0.8rem 1.2rem #00000030;
-    top: 100%;
+    background: var(--background, rgba(var(--background-color), 1));
+    border-radius: var(--border-radius, 0.5rem);
+    z-index: 1;
+    -webkit-box-shadow: 0 0.5rem 1.5rem -0.5rem rgba(0,0,0,0.3);
+            box-shadow: 0 0.5rem 1.5rem -0.5rem rgba(0,0,0,0.3);
     bottom: auto;
 }
-.moveUp{
-    top: auto;
-    bottom: 100%;
-    -webkit-transform: translateY(3rem);
-        -ms-transform: translateY(3rem);
-            transform: translateY(3rem);
+.hide{
+    display: none;
 }
-.moveLeft{
-    left: auto;
-    right: 0;
-}
-.no-transformations{
-    -webkit-transform: none !important;
-        -ms-transform: none !important;
-            transform: none !important;
+@media screen and (max-width: 640px){
+    .options{
+        position: fixed;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        top: auto;
+        border-radius: 0.5rem 0.5rem 0 0;
+    }
 }
 @media (hover: hover){
-    .menu:hover{
+    .menu:hover .icon{
         background: rgba(var(--text-color), 0.1); 
     }
 }
 </style>
 <div class="select">
-    <button class="menu">
-       <slot name="icon">
+    <div class="menu" tabindex="0">
         <svg class="icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/></svg>
-       </slot>
-    </button>
+    </div>
     <div class="options hide">
         <slot></slot> 
     </div>
@@ -2544,11 +2522,25 @@ customElements.define('sm-menu', class extends HTMLElement {
         this.attachShadow({
             mode: 'open'
         }).append(smMenu.content.cloneNode(true))
+
+        this.isOpen = false;
         this.availableOptions
         this.containerDimensions
+        this.animOptions = {
+            duration: 200,
+            easing: 'ease'
+        }
+
         this.optionList = this.shadowRoot.querySelector('.options')
         this.menu = this.shadowRoot.querySelector('.menu')
-        this.open = false;
+        this.icon = this.shadowRoot.querySelector('.icon')
+
+        this.expand = this.expand.bind(this)
+        this.collapse = this.collapse.bind(this)
+        this.toggle = this.toggle.bind(this)
+        this.handleKeyDown = this.handleKeyDown.bind(this)
+        this.handleClickOutside = this.handleClickOutside.bind(this)
+
     }
     static get observedAttributes() {
         return ['value']
@@ -2559,82 +2551,112 @@ customElements.define('sm-menu', class extends HTMLElement {
     set value(val) {
         this.setAttribute('value', val)
     }
-    expand = () => {
-        if (!this.open) {
+    expand() {
+        if (!this.isOpen) {
             this.optionList.classList.remove('hide')
-            this.optionList.classList.add('no-transformations')
-            this.menu.classList.add('focused')
-            this.open = true
-            this.availableOptions.forEach(option => {
-                option.setAttribute('tabindex', '0')
-            })
+            this.optionList.animate([
+                {
+                    transform: window.innerWidth < 640 ? 'translateY(1.5rem)' : 'translateY(-1rem)',
+                    opacity: '0'
+                },
+                {
+                    transform: 'none',
+                    opacity: '1'
+                },
+            ], this.animOptions)
+                .onfinish = () => {
+                    this.isOpen = true
+                    this.icon.classList.add('focused')
+                }
         }
     }
     collapse() {
-        if (this.open) {
-            this.open = false
-            this.optionList.classList.add('hide')
-            this.optionList.classList.remove('no-transformations')
-            this.menu.classList.remove('focused')
-            this.availableOptions.forEach(option => {
-                option.removeAttribute('tabindex')
-            })
+        if (this.isOpen) {
+            this.optionList.animate([
+                {
+                    transform: 'none',
+                    opacity: '1'
+                },
+                {
+                    transform: window.innerWidth < 640 ? 'translateY(1.5rem)' : 'translateY(-1rem)',
+                    opacity: '0'
+                },
+            ], this.animOptions)
+                .onfinish = () => {
+                    this.isOpen = false
+                    this.icon.classList.remove('focused')
+                    this.optionList.classList.add('hide')
+                }
         }
     }
-    connectedCallback() {
-        let slot = this.shadowRoot.querySelector('.options slot')
-        this.menu.addEventListener('click', e => {
-            if (!this.open) {
-                this.expand()
-            } else {
-                this.collapse()
-            }
-        })
-        this.menu.addEventListener('keydown', e => {
-            if (e.code === 'ArrowDown' || e.code === 'ArrowRight') {
+    toggle() {
+        if (!this.isOpen) {
+            this.expand()
+        } else {
+            this.collapse()
+        }
+    }
+    handleKeyDown(e) {
+        // If key is pressed on menu button
+        if (e.target === this) {
+            if (e.key === 'ArrowDown') {
                 e.preventDefault()
                 this.availableOptions[0].focus()
             }
-            if (e.code === 'Enter' || e.code === 'Space') {
+            else if (e.key === 'Enter' || e.key === ' ') {
                 e.preventDefault()
-                if (!this.open) {
-                    this.expand()
-                } else {
-                    this.collapse()
-                }
+                this.toggle()
             }
-        })
-        this.optionList.addEventListener('keydown', e => {
-            if (e.code === 'ArrowUp' || e.code === 'ArrowRight') {
+        } else { // If key is pressed over menu options
+            if (e.key === 'ArrowUp') {
                 e.preventDefault()
                 if (document.activeElement.previousElementSibling) {
                     document.activeElement.previousElementSibling.focus()
+                } else {
+                    this.availableOptions[this.availableOptions.length - 1].focus()
                 }
             }
-            if (e.code === 'ArrowDown' || e.code === 'ArrowLeft') {
+            else if (e.key === 'ArrowDown') {
                 e.preventDefault()
-                if (document.activeElement.nextElementSibling)
+                if (document.activeElement.nextElementSibling) {
                     document.activeElement.nextElementSibling.focus()
+                } else {
+                    this.availableOptions[0].focus()
+                }
             }
-        })
-        this.optionList.addEventListener('click', e => {
+            else if (e.key === 'Enter' || e.key === ' ') {
+                e.preventDefault()
+                e.target.click()
+            }
+        }
+    }
+    handleClickOutside(e) {
+        if (!this.contains(e.target) && e.button !== 2) {
             this.collapse()
-        })
+        }
+    }
+    connectedCallback() {
+        this.setAttribute('role', 'listbox')
+        this.setAttribute('aria-label', 'dropdown menu')
+        const slot = this.shadowRoot.querySelector('.options slot')
         slot.addEventListener('slotchange', e => {
-            this.availableOptions = slot.assignedElements()
+            this.availableOptions = e.target.assignedElements()
             this.containerDimensions = this.optionList.getBoundingClientRect()
         });
-        window.addEventListener('mousedown', e => {
-            if (!this.contains(e.target) && e.button !== 2) {
-                this.collapse()
-            }
-        })
+        this.addEventListener('click', this.toggle)
+        this.addEventListener('keydown', this.handleKeyDown)
+        document.addEventListener('mousedown', this.handleClickOutside)
+    }
+    disconnectedCallback() {
+        this.removeEventListener('click', this.toggle)
+        this.removeEventListener('keydown', this.handleKeyDown)
+        document.removeEventListener('mousedown', this.handleClickOutside)
     }
 })
 
 // option
-const smMenuOption = document.createElement('template')
-smMenuOption.innerHTML = `
+const menuOption = document.createElement('template')
+menuOption.innerHTML = `
 <style>     
 *{
     padding: 0;
@@ -2648,17 +2670,17 @@ smMenuOption.innerHTML = `
     display: flex;
 }
 .option{
-    opacity: 0.9;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
     min-width: 100%;
-    padding: 0.6rem 2rem;
+    padding: var(--padding, 0.6rem 1rem);
     cursor: pointer;
     overflow-wrap: break-word;
     white-space: nowrap;
     outline: none;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
     user-select: none;
+    border-radius: 0.3rem;
     -webkit-box-align: center;
         -ms-flex-align: center;
             align-items: center;
@@ -2667,9 +2689,11 @@ smMenuOption.innerHTML = `
     outline: none;
     background: rgba(var(--text-color), 0.1);
 }
-@media (hover: hover){
+@media (any-hover: hover){
+    .option{
+        transition: background-color 0.2s;
+    }
     .option:hover{
-        opacity: 1;
         background: rgba(var(--text-color), 0.1);
     }
 }
@@ -2677,17 +2701,19 @@ smMenuOption.innerHTML = `
 <div class="option">
     <slot></slot> 
 </div>`;
-customElements.define('sm-menu-option', class extends HTMLElement {
+customElements.define('menu-option', class extends HTMLElement {
     constructor() {
         super()
         this.attachShadow({
             mode: 'open'
-        }).append(smMenuOption.content.cloneNode(true))
+        }).append(menuOption.content.cloneNode(true))
     }
 
     connectedCallback() {
+        this.setAttribute('role', 'option')
+        this.setAttribute('tabindex', '0')
         this.addEventListener('keyup', e => {
-            if (e.code === 'Enter' || e.code === 'Space') {
+            if (e.key === 'Enter' || e.key === ' ') {
                 e.preventDefault()
                 this.click()
             }
