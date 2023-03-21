c9eee532ecf5eeb64953d9d28a6c5b43ab720b19
sairaj mote <sairajmote3@gmail.com>
2023-03-21 18:23:18 +0530

    Bug fixes

--- scripts/messenger.js
+++ scripts/messenger.js
@@ -1144,7 +1144,9 @@
                         multsigs[addr] = {
                             redeemScript: decode.redeemscript,
                             pubKeys: decode.pubkeys,
-                            minRequired: decode.signaturesRequired
+                            minRequired: decode.signaturesRequired,
+                            time: result[i].time,
+                            txid: result[i].txid
                         }
                 }
                 resolve(multsigs);
@@ -1321,7 +1323,7 @@
                     requestPipelineInbox(pipeline.id, pipeline.model);
                     resolve(_loaded.pipeline[pipeline.id])
                 }).catch(error => reject(error))
-            })
+            }).catch(error => reject(error))
         })
     }
 
