149ba17ddee9f8ffa40b0ff08e92ee6bb8ef9ad2
tripathyr <rohit.tripathy@gmail.com>
2023-10-19 17:08:22 +0530

    Update messengerEthereum.js

--- scripts/messengerEthereum.js
+++ scripts/messengerEthereum.js
@@ -6,17 +6,10 @@
 
 // onlyEvenY is usually false. It is needed to be true only when taproot private keys are input
 const ethAddressFromPrivateKey = floEthereum.ethAddressFromPrivateKey = function(privateKey, onlyEvenY = false){
-    var t1,t1_x,t1_y,t1_y_BigInt,t2,t3,t4;
-    var groupOrder = BigInt("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F");
-
+    var t1,t1_x,t1_y,t2,t3,t4;
     t1 = bitjs.newPubkey(privateKey);
     t1_x = t1.slice(2, 66); t1_y = t1.slice(-64);
-    
-    if (onlyEvenY) {
-    t1_y_BigInt = BigInt("0x"+t1_y); 
-    if (t1_y_BigInt % 2n !== 0n) { t1_y_BigInt = (groupOrder-t1_y_BigInt)%groupOrder; t1_y=t1_y_BigInt.toString(16)};
-    }
-    
+           
     t2 = t1_x.toString(16) + t1_y.toString(16);
     t3 = keccak.keccak_256(Crypto.util.hexToBytes(t2));
     t4 = keccak.extractLast20Bytes(t3);
